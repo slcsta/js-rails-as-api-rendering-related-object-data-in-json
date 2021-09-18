1a2cc45cde22e2ad0d2591fb555a5add650db339
Sarah <sarahlchavez@gmail.com>
2021-09-18 10:51:32 -0700

    commit tree b9fcfa8e3de4143d0f66aa651ed3952664091290

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,7 @@
+class SightingsController < ApplicationController
+
+    def show
+        sighting = Sighting.find_by(id: params[:id])
+        render json: { id: sighting.id, bird: sighting.bird, location: sighting.location }
+    end
+end
